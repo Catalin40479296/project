786f904a1fb7df03d212faab46d6f7671b8c8c76
Catalin <catalinleonte92@gmail.com>
2021-12-10 13:55:36 +0000

    v3.3

--- auth.py
+++ auth.py
@@ -2,7 +2,9 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from models import User
-from init import db
+from init import db, create_app
+
+app = create_app()
 
 auth = Blueprint('auth', __name__)
 

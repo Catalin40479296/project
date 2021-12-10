51770934ceca71378c34ecb227aaf351cc4a4536
Catalin <catalinleonte92@gmail.com>
2021-12-10 13:58:08 +0000

    v3.4

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from datetime import date
 from init import db, create_app
 
 from models import ActiveWorkouts
+app=create_app()
 
 main = Blueprint('main', __name__)
 
-app=create_app()
+
 
 #index page, rendering active workouts and giving date, workouts found on the user id and the name of the use
 @main.route('/')
